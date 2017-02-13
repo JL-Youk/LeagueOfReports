606c14c9da06c751e6f24a9c3d1cd0ca81540e66
JL <jeanlouis02300@gmail.com>
2017-02-13 15:35:34 +0100

    debut design moteur de recherche

--- js/init.js
+++ js/init.js
@@ -3,6 +3,14 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('input.autocomplete').autocomplete({
+  data: {
+    "Youkoulailay": null,
+    "x Chaton mignon": null,
+    "Thegreenradis": null,
+    "The daif": null,
+  },
+  limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+  });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
