3683a8769abcb30bc71ffbdd6426c23977f574ce
JL <jeanlouis02300@gmail.com>
2017-02-14 11:35:03 +0100

    sups de l'autocompletation dans le .js

--- js/init.js
+++ js/init.js
@@ -2,21 +2,10 @@
   $(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('input.autocomplete').autocomplete({
-      data: {
-        "Youkoulailay": null,
-        "x Chaton mignon": null,
-        "Thegreenradis": null,
-        "The daif": null,
-        "ericzemmour": null,
-        "spartanjohn117": null,
-      },
-      limit: 4, // The max amount of results that can be shown at once. Default: Infinity.
-    });
     $( "#button_report_1" ).click(function() {
     $( "#formulaire_report" ).slideDown( "slow");
     $( "#index-banner" ).fadeOut( "slow");
     $( "#info_site" ).fadeOut( "slow");
-});
+  });
 }); // end of document ready
 })(jQuery); // end of jQuery name space
